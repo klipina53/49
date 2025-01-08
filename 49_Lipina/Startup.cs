66e4605060a3cf092a6d09268412338caaf8c507
klipina53 <klipina53@gmail.com>
2025-01-08 19:12:01 +0500

    Подключение swagger.Lipina

--- 49_Lipina/Startup.cs
+++ 49_Lipina/Startup.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,29 +14,55 @@ namespace _49_Lipina
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Руководство для использования запросов",
+                    Description = "Полное руководство для использования запросов, находящихся в проекте"
+                });
+                c.SwaggerDoc("v2", new OpenApiInfo
+                {
+                    Version = "v2",
+                    Title = "Руководство для использования запросов",
+                    Description = "Полное руководство для использования запросов, находящихся в проекте"
+                });
+                c.SwaggerDoc("v3", new OpenApiInfo
+                {
+                    Version = "v3",
+                    Title = "Руководство для использования запросов",
+                    Description = "Полное руководство для использования запросов, находящихся в проекте"
+                });
+                c.SwaggerDoc("v4", new OpenApiInfo
+                {
+                    Version = "v4",
+                    Title = "Руководство для использования запросов",
+                    Description = "Полное руководство для использования запросов, находящихся в проекте"
+                });
+                var filePath = Path.Combine(System.AppContext.BaseDirectory, "pr49-RestApi_Pleshkova.StaticWebAssets.xml");
+                c.IncludeXmlComments(filePath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseMvcWithDefaultRoute();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Регистрация и авторизация пользователя");
+                c.SwaggerEndpoint("/swagger/v2/swagger.json", "Список версий");
+                c.SwaggerEndpoint("/swagger/v3/swagger.json", "Список блюд");
+                c.SwaggerEndpoint("/swagger/v4/swagger.json", "Отправка заказа и получение истории");
             });
         }
     }
 }
+    
