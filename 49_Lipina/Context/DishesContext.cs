47269caec902200917aa44482f65474f4efc44d9
klipina53 <klipina53@gmail.com>
2025-01-08 20:16:42 +0500

    Разработанные контексты для загрузки данных. Липина

---
+++ 49_Lipina/Context/DishesContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using _49_Lipina.Models;
+using System;
+
+namespace _49_Lipina.Context
+{
+    public class DishesContext : DbContext
+    {
+        public DbSet<Dishes> Dishes { get; set; }
+        public DishesContext()
+        {
+            Database.EnsureCreated();
+            Dishes.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;" +
+                "uid=root;" +
+                "port=3303;" +
+                "pwd=;" +
+                "database=RestApi", new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
---
+++ 49_Lipina/Context/UsersContext.cs
@@ -0,0 +1,24 @@
+﻿    using Microsoft.EntityFrameworkCore;
+using _49_Lipina.Models;
+using System;
+
+namespace _49_Lipina.Context
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public UsersContext()
+        {
+            Database.EnsureCreated();
+            Users.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;" +
+                "uid=root;" +
+                "port=3303;" +
+                "pwd=;" +
+                "database=RestApi", new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
